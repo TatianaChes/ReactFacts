7ab37c7df630f371fd7568124ec9fbfd668cb427
TatianaChes <tatianaches49@gmail.com>
2024-09-17 12:45:52 +0500

    change header title and add screen image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
         <nav>
             <img src={reactIcon} className="nav--icon" />
             <h3 className="nav--logo_text">ReactFacts</h3>
-            <h4 className="nav--title">React Course - Project 1</h4>
+            <h4 className="nav--title">React Project</h4>
         </nav>
     )
 }
